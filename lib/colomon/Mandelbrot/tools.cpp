8034030f71173400003c760cb39602abfc02ed4e
Solomon Foster <colomon@gmail.com>
2020-10-14 21:40:00 -0400

    Start sketching in C++ tools.

---
+++ lib/colomon/Mandelbrot/tools.cpp
@@ -0,0 +1,16 @@
+#include <complex>
+
+unsigned int
+mandel_double(double re, double im, unsigned int max_iterations)
+{
+    std::complex<double> c (re, im);
+    std::complex<double> z (0.0, 0.0);
+    for (unsigned int i = 0; i < max_iterations; i++)
+    {
+        if (std::abs(z) > 2) {
+            return i + 1;
+        }
+        z = z * z + c;
+    }
+    return 0;
+}
